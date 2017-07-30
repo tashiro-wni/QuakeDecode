f1b58e2e80484f2fdbec039fdc217e2d38cffe81
Tomohiro Tashiro <tomohiro.tashiro@gmail.com>
2017-07-30 21:45:09 +0900

    initial commit

---
+++ QuakeDecode/Quake.swift
@@ -0,0 +1,53 @@
+//
+//  Quake.swift
+//  QuakeDecode
+//
+//  Created by Tomohiro Tashiro on 7/30/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+import Foundation
+
+struct QuakeList : Decodable {
+    let list :[Quake]
+    private enum CodingKeys : String, CodingKey {
+        case list = "quake"
+    }
+}
+
+struct Quake : Decodable {
+    let name :String
+    let spot :String
+    let cell_atime :String
+    let atimeString :String
+    let latlon :String
+    let magnitude :String
+    let depth :String
+    let maxclass :String
+    let wave :String
+    let points :String
+    let image :String
+}
+
+class QuakeLoader {
+    static let api = "https://gist.githubusercontent.com/tashiro-wni/b9f66b5841edacd77cae02eb8c8fd329/raw/5659624003751ea9ceed561bf7fc743ffb93a545/quake.json"
+    
+    class func get(completion: @escaping (_ quakeList: [Quake]?) -> Void) {
+        let task = URLSession.shared.dataTask(with: URL(string: api)!) { (data, response, error) in
+            guard let data = data else {
+                print("http get error")
+                completion(nil)
+                return
+            }
+            
+            do {
+                let list = try JSONDecoder().decode(QuakeList.self, from: data)
+                completion(list.list)
+            } catch {
+                print("json decode error")
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
+}
---
+++ QuakeDecode/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  QuakeDecode
+//
+//  Created by Tomohiro Tashiro on 7/30/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var mTableView :UITableView!
+    private let cellIdentifier = "cellIdentifier"
+    private var quakeList :[Quake] = []
+    
+    //MARK: - viewController life cycle
+    deinit {
+        if mTableView != nil {
+            mTableView.delegate = nil
+            mTableView.dataSource = nil
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mTableView = UITableView(frame: self.view.bounds, style: .plain)
+        mTableView.delegate = self
+        mTableView.dataSource = self
+        mTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        self.view.addSubview(mTableView)
+        
+        QuakeLoader.get(completion: dataLoaded(_:))
+    }
+    
+    func dataLoaded(_ list: [Quake]?) {
+        guard let list = list else { print("load failed."); return }
+        
+        quakeList = list
+        DispatchQueue.main.async {
+            self.mTableView.reloadData()
+        }
+    }
+    
+    //MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quakeList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        if indexPath.row < quakeList.count {
+            let quake = quakeList[indexPath.row]
+            cell.textLabel?.text = "\(quake.cell_atime), \(quake.spot), M\(quake.magnitude), 最大震度:\(quake.maxclass)"
+        }
+        
+        return cell
+    }
+    
+}
+
